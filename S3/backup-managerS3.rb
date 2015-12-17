bab6ff40256e5427a752dbf327506754b1b16ded
wkisielewicz <wkisielewicz@criticue.com>
2015-12-17 16:31:59 +0100

    change paths

--- S3/backup-managerS3.rb
+++ S3/backup-managerS3.rb
@@ -1,8 +1,8 @@
 #duplicity /media/kisiel/Data/VirtualBox\ VMs s3+http://backup-virtualmachines-protonred
 require 'optparse'
 #CONFIG BEGIN
-PATHS=[{:source =>"'/media/kisiel/Data/VirtualBox\ VMs/FirebirdUbuntuTest'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/FirebirdUbuntuTest", :unic_name =>"FirebirdUbuntuTest"},
-       {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/oracle-linux-data'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/oracle-linux-data", :unic_name =>"oracle-linux-data"},
+PATHS=[{:source =>"'/home/kisiel/NAS/wioletta/VMs/FirebirdUbuntuTest'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/FirebirdUbuntuTest", :unic_name =>"FirebirdUbuntuTest"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/oracle-linux-data'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/oracle-linux-data", :unic_name =>"oracle-linux-data"},
        {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/Win8.1'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/Win8.1", :unic_name =>"Win8.1"},
        {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/WindowsServer2012'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/WindowsServer2012", :unic_name =>"WindowsServer2012"},
        {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/WinServer2008'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/WinServer2008", :unic_name =>"winServer"},
