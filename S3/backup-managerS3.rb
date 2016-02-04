26940f60a394495007fd0810d8fbce0c117a87a1
wkisielewicz <wkisielewicz@criticue.com>
2016-02-04 10:17:11 +0100

    change folders/ create group

--- S3/backup-managerS3.rb
+++ S3/backup-managerS3.rb
@@ -1,20 +1,19 @@
 #duplicity /media/kisiel/Data/VirtualBox\ VMs s3+http://backup-virtualmachines-protonred
 require 'optparse'
 #CONFIG BEGIN
-PATHS=[{:source =>"'/home/kisiel/NAS/wioletta/VMs/FirebirdUbuntuTest'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/FirebirdUbuntuTest", :unic_name =>"FirebirdUbuntuTest"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/oracle-linux-data'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/oracle-linux-data", :unic_name =>"oracle-linux-data"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Win8.1'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1", :unic_name =>"Win8.1"},
-       {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/WindowsServer2012'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/WindowsServer2012", :unic_name =>"WindowsServer2012"},
-       {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/WinServer2008'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/Testowe/WinServer2008", :unic_name =>"winServer"},
+PATHS=[{:source =>"'/home/kisiel/NAS/wioletta/VMs/Testy_instalek/FirebirdUbuntuTest'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/FirebirdUbuntuTest", :unic_name =>"FirebirdUbuntuTest"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Testy_instalek/oracle-linux-data'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/oracle-linux-data", :unic_name =>"oracle-linux-data"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Testowe/Win8.1'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1", :unic_name =>"Win8.1"},
+       {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/Testowe/WindowsServer2012'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/WindowsServer2012", :unic_name =>"WindowsServer2012"},
+       {:source =>"'/media/kisiel/Data/VirtualBox\ VMs/Testowe/WinServer2008'", :destination =>"s3+http://backup-virtualmachines-protonred/VirtualBoxVMs/Testowe/WinServer2008", :unic_name =>"winServer"},
        {:source =>"'/media/kisiel/Data/virtual\ disc\ linux\ oracle'", :destination =>"s3+http://backup-virtualmachines-protonred/virtual-disc-linux-oracle", :unic_name =>"virtual-disc-linux-oracle"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Build'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Build/Build", :unic_name =>"Build"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Oracle_Linux_32bit'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/Oracle_Linux_32bit", :unic_name =>"Oracle_Linux_32bit"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Ubuntu32bit_Test_Installers'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/Ubuntu32bit_Test_Installers", :unic_name =>"Ubuntu32bit_Test_Installers"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/win8.1_oracle_test'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Oracle/win8.1_oracle_test", :unic_name =>"win8.1_oracle_test"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Win8.1_12c_oracle'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Oracle/Win8.1_12c_oracle", :unic_name =>"Win8.1_12c_oracle"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Win8.1_automate'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1_automate", :unic_name =>"Win8.1_automate"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Win8.1_server'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1_server", :unic_name =>"Win8.1_server"},
-       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Win8.1_Test_Firendly_name'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1_Test_Firendly_name", :unic_name =>"Win8.1_Test_Firendly_name"}]
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Build/Build'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Build/Build", :unic_name =>"Build"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Testy_instalek/Oracle_Linux_32bit'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/Oracle_Linux_32bit", :unic_name =>"Oracle_Linux_32bit"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Testy_instalek/Ubuntu32bit_Test_Installers'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testy_instalek/Ubuntu32bit_Test_Installers", :unic_name =>"Ubuntu32bit_Test_Installers"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Oracle/win8.1_oracle_test'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Oracle/win8.1_oracle_test", :unic_name =>"win8.1_oracle_test"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Oracle/Win8.1_12c_oracle'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Oracle/Win8.1_12c_oracle", :unic_name =>"Win8.1_12c_oracle"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Testowe/Win8.1_automate'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1_automate", :unic_name =>"Win8.1_automate"},
+       {:source =>"'/home/kisiel/NAS/wioletta/VMs/Testowe/Win8.1_server'", :destination =>"s3+http://backup-virtualmachines-protonred/VMs/Testowe/Win8.1_server", :unic_name =>"Win8.1_server"}]
 
 # SET ENVIROMENT PASSHRASE export PASSPHRASE='password'
 
